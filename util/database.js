7c28e1879517b9ba026d51264776b9e6dbe802bc
Lê Vĩnh Long - CMC Global DU12 <lvlong@ldap.cmcglobal.com.vn>
2021-04-20 14:37:39 +0700

    1st commit

---
+++ util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://lvlong:Icarus0208@cluster0.f64r8.mongodb.net/lvl-demo-graphql?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+exports.MongoConnect = mongoConnect;
+exports.getDb = getDb;
